3ce99a2ac5bd5937d8810b7a1ac73fad56615504
nanangcreacoll <nanang.creacol.26@gmail.com>
2024-04-12 00:33:01 +0700

    fix: login errors request

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,19 +14,29 @@
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-19 col-md-5 col-lg-6 col-xl-4">
-                <img class="logo-poltek mb-4 mt-4 center-block" src="{{ asset('img/Vertikal_1.png') }}" alt="logo poltek nuklir"
-                    width="35%" height="35%">
+                <img class="logo-poltek mb-4 mt-4 center-block" src="{{ asset('img/Vertikal_1.png') }}"
+                    alt="logo poltek nuklir" width="35%" height="35%">
                 <h1 class="h3 mb-3 fw-normal text-white text-center"><strong>Login</strong></h1>
                 <div class="card bg-white text-black" style="border-radius: 1rem;">
                     <div class="card-body p-2 text-center">
                         @if (session()->has('loginError'))
                             <div class="alert alert-danger alert-dismissible fade show" role="alert">
                                 {{ session('loginError') }}
-                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                    aria-label="Close"></button>
                             </div>
                         @endif
+                        @if ($errors->any())
+                            @foreach ($errors->all() as $error)
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    {{ $error }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                                </div>
+                            @endforeach
+                        @endif
                         <main class="card-body form-signin w-100 m-auto">
-                            <form action="/login" method="post">
+                            <form action="{{ route('login-auth') }}" method="post">
                                 @csrf
                                 <div class="form-floating text-dark-emphasis">
                                     <input type="text" name="username" class="form-control" id="username"
@@ -39,12 +49,14 @@
                                     <label for="password">Password</label>
                                 </div>
                                 <div class="form-check text-start my-3">
-                                    <input class="form-check-input" type="checkbox" name="remember-me" value="remember-me" id="remember-me">
+                                    <input class="form-check-input" type="checkbox" name="remember-me"
+                                        value="remember-me" id="remember-me">
                                     <label class="form-check-label" for="remember-me">
-                                      Remember me
+                                        Remember me
                                     </label>
-                                  </div>
-                                <button class="btn btn-success mt-4 bg-gradient w-100 py-2" type="submit">Login</button>
+                                </div>
+                                <button class="btn btn-success mt-4 bg-gradient w-100 py-2"
+                                    type="submit">Login</button>
                             </form>
                         </main>
                     </div>
