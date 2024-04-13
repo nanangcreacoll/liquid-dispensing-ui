84fc2dc77ed71a32510772c2d5f054f30f908ebd
nanangcreacoll <nanang.creacol.26@gmail.com>
2024-04-14 00:51:10 +0700

    feat: add tab icon

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel = "icon" href = "{{ asset('img/LogoGram_1.png') }}" type = "image/x-icon">
     <link href="{{ asset('dist/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     @vite('resources/css/app.css')
     <link href="{{ asset('dist/sb-admin-2/vendor/datatables/dataTables.bootstrap4.css') }}" rel="stylesheet">
