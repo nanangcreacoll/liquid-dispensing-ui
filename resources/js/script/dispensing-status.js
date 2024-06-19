4ac7997f2fa856ea114e178c67e0f5dac1044095
nanangcreacoll <nanang.creacol.26@gmail.com>
2024-06-19 20:15:31 +0700

    fix: js script excecution

--- resources/js/script/dispensing-status.js
+++ resources/js/script/dispensing-status.js
@@ -1,17 +1,19 @@
 $(function () {
-    const dispensingStatusSession = $('meta[name="dispensing-status-session"]').attr("content");
-    const statusCircle = document.getElementById("status-circle");
-    const dispensingStartButton = document.getElementById("dispensingStartButton");
+    if (window.location.pathname === '/kendali') {
+        const dispensingStatusSession = $('meta[name="dispensing-status-session"]').attr("content");
+        const statusCircle = document.getElementById("status-circle");
+        const dispensingStartButton = document.getElementById("dispensingStartButton");
 
-    function updateStatus(dispensing) {
-        statusCircle.classList.toggle("bg-danger", !dispensing);
-        statusCircle.classList.toggle("bg-success", dispensing);
-        dispensingStartButton.classList.toggle("disabled", !dispensing);
+        function updateStatus(dispensing) {
+            statusCircle.classList.toggle("bg-danger", !dispensing);
+            statusCircle.classList.toggle("bg-success", dispensing);
+            dispensingStartButton.classList.toggle("disabled", !dispensing);
+        }
+
+        updateStatus(dispensingStatusSession === "true");
+
+        window.Echo.channel("dispensing-status").listen("DispensingStatus", (event) => {
+            updateStatus(event.status);
+        });
     }
-
-    updateStatus(dispensingStatusSession === "true");
-
-    window.Echo.channel("dispensing-status").listen("DispensingStatus", (event) => {
-        updateStatus(event.status);
-    });
 });
--- resources/js/script/password-toggle.js
+++ resources/js/script/password-toggle.js
@@ -1,16 +1,18 @@
 $(function() {
-    document.getElementById('togglePassword').addEventListener('click', function() {
-        const passwordField = document.getElementById('password');
-        const eyeIcon = document.getElementById('eyeIcon');
-        const eyeSlashIcon = document.getElementById('eyeSlashIcon');
-        if (passwordField.type === 'password') {
-            passwordField.type = 'text';
-            eyeIcon.style.display = 'none';
-            eyeSlashIcon.style.display = 'block';
-        } else {
-            passwordField.type = 'password';
-            eyeIcon.style.display = 'block';
-            eyeSlashIcon.style.display = 'none';
-        }
-    });
+    if (window.location.pathname === '/login') {
+        document.getElementById('togglePassword').addEventListener('click', function() {
+            const passwordField = document.getElementById('password');
+            const eyeIcon = document.getElementById('eyeIcon');
+            const eyeSlashIcon = document.getElementById('eyeSlashIcon');
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                eyeIcon.style.display = 'none';
+                eyeSlashIcon.style.display = 'block';
+            } else {
+                passwordField.type = 'password';
+                eyeIcon.style.display = 'block';
+                eyeSlashIcon.style.display = 'none';
+            }
+        });
+    }
 });
